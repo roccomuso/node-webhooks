a14a44d0f14219d7d85ba283362be94d2fdf4c66
Rocco Musolino <rocco@hackerstribe.com>
2016-02-06 20:45:12 +0100

    beta released.

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ DB Structure Example:
 
 */
 
+var jsonfile = require('jsonfile');
+var fs = require('fs');
 var crypto = require('crypto');
 var request = require('request');
 var events = require('events');
@@ -23,89 +25,84 @@ var _functions = {};
 
 // WebHooks Class
 function WebHooks(options){
-	if (typeof str !== 'object') throw new TypeError('Expected an Object');
+	if (typeof options !== 'object') throw new TypeError('Expected an Object');
 	if (typeof options.db !== 'string') throw new TypeError('db Must be a String path');
 
 
 	this.DEBUG = (typeof options.DEBUG === 'boolean') ? options.DEBUG : false;
 	this.db = options.db;
 
-	fs.access(this.db, fs.R_OK | fs.W_OK, function (err) {
-		if (typeof err !== 'undefined' && err.hasOwnProperty('code')){
-			if (err.code === 'ENOENT') // file not found, init DB:
-				_initDB(this.db).catch(function(err){ throw new Error(err); });
-		}else{
-			// DB already exists, set listeners for every URL.
-			_setListeners(this).catch(function(err){ throw new Error(err); });
-
-		}
-	});
-	
+	var self = this;
+	// sync loading:
+	try{
+		fs.accessSync(this.db, fs.R_OK | fs.W_OK);
+		//DB already exists, set listeners for every URL.
+		if (self.DEBUG) console.log('webHook DB loaded, setting listeners...');
+		_setListeners(self);
+	} catch(e){ // DB file not found, initialize it
+		if (e.hasOwnProperty('code')){
+			if (e.code === 'ENOENT'){ // file not found, init DB:
+				if (self.DEBUG) console.log('webHook DB init');
+				_initDB(self.db);
+			} else console.error(e);
+		} else console.error(e);
+	}
 
 }
 
 function _initDB(file){
 	// init DB.
-	return new Promise(function(resolve, reject){
-	  	var db = {}; // init empty db
-		jsonfile.writeFile(file, db, {spaces: 2}, function(err) {
-		  if (err) reject(err);
-		  else resolve();
-		});
-	});
+	 var db = {}; // init empty db
+	jsonfile.writeFileSync(file, db, {spaces: 2});
 }
 
 function _setListeners(self){
-	// set Listeners
-	return new Promise(function(resolve, reject){
-		jsonfile.readFile(self.db, function(err, obj) {
-			if (err) return reject(err);
-			for (var key in obj) {
-			    // skip loop if the property is from prototype
-			    if (!obj.hasOwnProperty(key)) continue;
+	// set Listeners - sync method
 
-			    var urls = obj[key];
-			    urls.forEach(function(url){
-			    	var enc_url = crypto.createHash('md5').update(url).digest("hex");
-			    	_functions[enc_url] = _getRequestFunction(self);
-			    	eventEmitter.on(key, _functions[enc_url]);
-			    });
-			}
-			resolve();
-		});
-	});
+	try{
+		var obj = jsonfile.readFileSync(self.db);
+		if (!obj) throw Error('can\'t read webHook DB content');
+
+		for (var key in obj) {
+		    // skip loop if the property is from prototype
+		    if (!obj.hasOwnProperty(key)) continue;
+
+		    var urls = obj[key];
+		    urls.forEach(function(url){
+		    	var enc_url = crypto.createHash('md5').update(url).digest('hex');
+		    	_functions[enc_url] = _getRequestFunction(self, url);
+		    	eventEmitter.on(key, _functions[enc_url]);
+		    });
+		}
+	}catch(e){
+		throw Error(e);
+	}
+
+	// console.log(_functions[0] == _functions[1]);
+	// console.log(_functions[1] == _functions[2]);
+	// console.log(_functions[0] == _functions[2]);
+	
 }
 
-function _getRequestFunction(self){
+function _getRequestFunction(self, url){
 	// return the function then called by the event listener.
-	var func = functions(shortname, json_data){ // 2 arguments required when eventEmitter.emit()
+	var func = function(json_data){ // argument required when eventEmitter.emit()
 
-		jsonfile.readFile(self.db, function(err, obj){
-			if (err && self.DEBUG) return console.error(err);
-			for (var key in obj){
-			    // skip loop if the property is from prototype
-			    if (!obj.hasOwnProperty(key)) continue;
+	    	if (self.DEBUG) console.log('POST request to:', url);
+			// POST request to the istantiated URL
+			request({
+			    method: 'POST',
+			    uri: url,
+			    strictSSL: false,
+			    headers: {'Content-Type': 'application/json'},
+			    body: JSON.stringify(json_data) 
+			  },
+			  function (error, response, body) {
+				    if ((error || response.statusCode !== 200 ) && self.DEBUG) return console.error('HTTP failed: '+ error);
+					if (self.DEBUG) console.log('Request sent - Server responded with:', body);
+			  }
+			);
 
-			    var urls = obj[key];
-			    urls.forEach(function(url){
-					// POST request to every URL if any.
-					if (url)
-						request({
-						    method: 'POST',
-						    uri: url,
-						    strictSSL: false,
-						    headers: {'cache-control': 'no-cache', 'Content-Type': 'application/json'},
-						    body: JSON.stringify(json_data) 
-						  },
-						  function (error, response, body) {
-							    if ((error || response.statusCode !== 200 ) && self.DEBUG) return console.error('HTTP failed: '+ error);
-								if (self.DEBUG) console.log('Request sent - Server responded with:', body);
-						  }
-						);
-
-			    });
-			}
-		});
 	};
 
 	return func;
@@ -114,18 +111,56 @@ function _getRequestFunction(self){
 // 'prototype' has improved performances, let's declare the methods
 
 WebHooks.prototype.trigger = function(shortname, json_data) {
-	// se c'è, altrimenti pazienza.
-	return new Promise(function(resolve, reject){
-		eventEmitter.emit(shortname, shortname, json_data);
-		resolve();
-	});
+	// trigger a webHook
+	eventEmitter.emit(shortname, json_data);
 };
 
 WebHooks.prototype.add = function(shortname, url) { // url is required
-	// TODO add a new webHook.
-	// se già c'è lo shortname si aggiunge la URL all'array.
+	// add a new webHook.
+	if (typeof shortname !== 'string') throw new TypeError('shortname required!');
+	if (typeof url !== 'string') throw new TypeError('Url must be a string');
+
+	var self = this;
 	return new Promise(function(resolve, reject){
-		// no blank spaces.
+		
+		try{
+
+				var obj = jsonfile.readFileSync(self.db);
+				if (!obj) throw Error('can\'t read webHook DB content');
+				
+				var modified = false;
+				if (obj[shortname]){
+					// shortname already exists
+					if (obj[shortname].indexOf(url) === -1){
+						// url doesn't exists for given shortname
+						if (self.DEBUG) console.log('url added to an existing shortname!');
+						obj[shortname].push(url);
+						var enc_url = crypto.createHash('md5').update(url).digest('hex');
+				    	_functions[enc_url] = _getRequestFunction(self, url);
+				    	eventEmitter.on(shortname, _functions[enc_url]);
+				    	modified = true;
+					}
+				}else{
+					// new shortname
+					if (self.DEBUG) console.log('new shortname!');
+					obj[shortname] = [url];
+					var enc_url = crypto.createHash('md5').update(url).digest('hex');
+				    _functions[enc_url] = _getRequestFunction(self, url);
+				    eventEmitter.on(shortname, _functions[enc_url]);
+				    modified = true;
+				}
+
+				// actualize DB
+				if (modified){
+					jsonfile.writeFileSync(self.db, obj);
+					  resolve(true);
+				} else resolve(false);
+
+
+
+	}catch(e){
+		reject(e);
+	}
 
 	});
 };
@@ -133,74 +168,97 @@ WebHooks.prototype.add = function(shortname, url) { // url is required
 WebHooks.prototype.remove = function(shortname, url) { // url is optional
 	// if url exists remove only the url attached to the selected webHook.
 	// else remove the webHook and all the attached URLs.
+	if (typeof shortname === 'undefined') throw new TypeError('shortname required!');
 	var self = this;
 	return new Promise(function(resolve, reject){
 		// Basically removeListener will look up the given function by reference, if it found that function it will remove it from the event hander.
-		var url_key = crypto.createHash('md5').update(url).digest("hex");
-		if (typeof url !== 'undefined'){
-			// remove only the specified url
-			eventEmitter.removeListener(shortname, _functions[url_key]);
-			delete _functions[url_key];
-			// save in db
-			_removeUrlFromShortname(self, shortname, url, function(err){
-				if (err) return reject(err);
-				resolve(true);
-			});
+		try{
+			if (typeof url !== 'undefined'){
 
-		}else{
-			// remove every event listener attached to the webHook shortname.
-			eventEmitter.removeAllListeners(shortname);
-
-			// delete all the callbacks in _functions for the specified shortname. Let's loop over the url taken from the DB.
-			jsonfile.readFile(self.db, function(err, obj){
-				if (err && self.DEBUG) return reject(err);
-				var urls = obj[shortname];
-				urls.forEach(function(url){
-					var url_key = crypto.createHash('md5').update(url).digest("hex");
-					delete _functions[url_key];
+				// save in db
+				_removeUrlFromShortname(self, shortname, url, function(err, done){
+					if (err) return reject(err);
+					if (done){
+						// remove only the specified url
+						var url_key = crypto.createHash('md5').update(url).digest('hex');
+						eventEmitter.removeListener(shortname, _functions[url_key]);
+						delete _functions[url_key];
+						resolve(true);
+					}
+					else resolve(false);
 				});
-			});
 
-			// attualizzare nel DB
-			_removeShortname(self, shortname, function(err){
-				if (err) return reject(err);
-				resolve(true);
-			});
+			}else{
+				// remove every event listener attached to the webHook shortname.
+				eventEmitter.removeAllListeners(shortname);
 
+				// delete all the callbacks in _functions for the specified shortname. Let's loop over the url taken from the DB.
+				var obj = jsonfile.readFileSync(self.db);
+
+				if (obj.hasOwnProperty(shortname)){
+					var urls = obj[shortname];
+					urls.forEach(function(url){
+						var url_key = crypto.createHash('md5').update(url).digest('hex');
+						delete _functions[url_key];
+					});
+
+					// save it back to the DB
+					_removeShortname(self, shortname, function(err){
+						if (err) return reject(err);
+						resolve(true);
+					});
+
+				}
+
+			}
+
+		}catch(e){
+			reject(e);
 		}
 
 	});
 };
 
 function _removeUrlFromShortname(self, shortname, url, callback){
-		var url_key = crypto.createHash('md5').update(url).digest("hex");
-		jsonfile.readFile(self.db, function(err, obj){
-			if (err && self.DEBUG) return console.error(err);
+
+	try{
+		var obj = jsonfile.readFileSync(self.db);
 			
-			obj[shortname] = obj[shortname].splice(obj[shortname].indexOf(url_key), 1);
+			var deleted = false;
+			var len = obj[shortname].length;
+			if (obj[shortname].indexOf(url) !== -1)
+				obj[shortname].splice(obj[shortname].indexOf(url), 1);
+			if (obj[shortname].length !== len) deleted = true;
 			// save it back to the DB
-			jsonfile.writeFile(self.db, obj, function (err) {
-			  if (err && self.DEBUG) callback(err);
-			  else callback(undefined);
-			});
-			
-		});
+			if (deleted){
+				jsonfile.writeFileSync(self.db, obj);
+				if (self.DEBUG) console.log('url removed from existing shortname');
+				callback(undefined, deleted);
+			} else callback(undefined, deleted);
+
+	}catch(e){
+		callback(e, undefined);
+	}
+
 }
 
 function _removeShortname(self, shortname, callback){
-		jsonfile.readFile(self.db, function(err, obj){
-			if (err && self.DEBUG) return console.error(err);
-			
-			delete obj[shortname];
-			// save it back to the DB
-			jsonfile.writeFile(self.db, obj, function (err) {
-			  if (err && self.DEBUG) callback(err);
-			  else callback(undefined);
-			});
-			
-		});
+	try{
+
+		var obj = jsonfile.readFileSync(self.db);
+		delete obj[shortname];
+		// save it back to the DB
+		jsonfile.writeFileSync(self.db, obj);
+		if (self.DEBUG) console.log('whole shortname urls removed');
+		callback(undefined);
+		
+	}catch(e){
+		callback(e);
+	}			
+		
 }
 
+// async method
 WebHooks.prototype.getDB = function() {
 	// return the whole JSON DB file.
 	var self = this;
@@ -219,18 +277,30 @@ WebHooks.prototype.getDB = function() {
 	});
 };
 
+// async method
 WebHooks.prototype.getWebHook = function(shortname) {
-	// TODO return the selected WebHook.
+	// return the selected WebHook.
+	var self = this;
 	return new Promise(function(resolve, reject){
-
+		jsonfile.readFile(self.db, function(err, obj) {
+			if (err){
+			  	if (err.code === 'ENOENT') // file not found
+					reject('file not found');
+				 else 
+				  	reject(err);
+			}else{
+				// file exists
+				if (obj[shortname])
+					resolve(obj[shortname]);
+				else
+					resolve({});
+			}
+		});
 	});
 };
 
-WebHooks.prototype.getDescription = function(shortname, url) { // url is required
-	// TODO return description for given shortname and url
-	return new Promise(function(resolve, reject){
-
-	});
+WebHooks.prototype.get_functions = function(){
+	return _functions;
 };
 
 module.exports = WebHooks;
